71eacdfa760427d4fe5422fb09b8daa95861dc0d
kabaskimy <kabaskimy@gmail.com>
2013-02-07 16:46:36 +0800

    测试

--- GitApp/MvcTest/Controllers/HomeController.cs
+++ GitApp/MvcTest/Controllers/HomeController.cs
@@ -19,5 +19,17 @@ namespace MvcTest.Controllers
             return View(order);
         }
 
+        public ActionResult Login()
+        {
+            User user = MvcTest.Models.User.GetDefaultUser();
+            return View(user);
+        }
+
+        public JsonResult UserExist(string UserName)
+        {
+            var result = MvcTest.Models.User.GetUser(UserName) == null;
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
