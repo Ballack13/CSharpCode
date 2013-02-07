3041237ae1db150cc4412ae78ada05608ce53bc0
kabaskimy <kabaskimy@gmail.com>
2013-02-07 16:46:22 +0800

    测试Model

--- GitApp/MvcTest/Models/Order.cs
+++ GitApp/MvcTest/Models/Order.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcTest.Models
 {
     public class Order
     {
-        
+        [Editable(false)]
         public int OrderId { get;private set; }
+
         public DateTime OrderDate { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public string UserName { get; set; }
+
+        [StringLength(10)]
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { set; get; }
---
+++ GitApp/MvcTest/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MvcTest.Models
+{
+    public class User
+    {
+        [Remote("UserExist","Home")]
+        public string UserName { set; get; }
+        public string Password { set; get; }
+
+        public static List<User> GetAllUser()
+        {
+            List<User> listUser = new List<User>();
+            listUser.Add(new User()
+            {
+                UserName = "Ballck",
+                Password = "123456"
+            });
+
+            listUser.Add(new User()
+            {
+                UserName = "Kahn",
+                Password = "123456"
+            });
+
+            listUser.Add(new User()
+            {
+                UserName = "Muller",
+                Password = "123456"
+            });
+            return listUser;
+        }
+        public static User GetUser(string UserName)
+        {
+            List<User> users = GetAllUser();
+            return users.First(t => t.UserName == UserName);
+        }
+
+        public static User GetDefaultUser()
+        {
+            List<User> users = GetAllUser();
+            return users.First(x => true);
+        }
+    }
+}
